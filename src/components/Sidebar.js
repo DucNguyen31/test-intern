a5ec572d73efd0c6e07c8bcf3c1fecd2741e619b
Nguyen Sy Duc <nguyensyduc0301@gmail.com>
2023-04-01 21:30:59 +0700

    components

---
+++ src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../Styles.css'
+
+function Sidebar() {
+  return (
+      <div className="sidebar">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">News</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+  );
+}
+
+export default Sidebar;
